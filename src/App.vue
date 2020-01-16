ebd343db9fd0697c664035a494cea44da9df7e07
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-16 13:24:00 +0000

    list of beer names appearing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <h1>Brew Dog Beers</h1>
+    <div class="main-container">
+      <all-beers-list :beers='beers'></all-beers-list >
+        <!-- <beer-detail :rendered-beer="selectedBeer"></beer-detail > -->
+          <!-- <favourite-beers-list :favourite-beers="favouriteBeers"></favourite-beers-list > -->
+          </div>
+        </div>
+      </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+      <script>
+      import AllBeersList from './components/AllBeersList.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+      export default {
+        name: 'app',
+        data() {
+          return {
+            beers: []
+            // favouriteBeers: [],
+            // selectedBeerId: null
+          }
+        },
+        mounted() {
+          fetch('https://api.punkapi.com/v2/beers')
+            .then(response => response.json())
+            .then(beers => this.beers = beers)
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+        },
+        components: {
+          "all-beers-list": AllBeersList
+          // "beer-detail": BeerDetail,
+          // "favourite-beers-list": FavouriteBeersList
+        }
+      }
+      </script>
+
+      <style>
+        /* #app {
+          font-family: 'Avenir', Helvetica, Arial, sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          text-align: center;
+          color: #2c3e50;
+          margin-top: 60px;
+        } */
+        .main-container {
+          display: flex;
+          justify-content: space-between;
+        }
+      </style>
---
+++ src/components/AllBeersList.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <ul>
+      <li v-for="beer in beers">{{beer.name}}</li>
+      <!-- <beer-list-item >  {{beers}}    </beer-list-item> -->
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import BeerListItem from './BeerListItem.vue'
+
+export default {
+  name: 'all-beers-list',
+  props: ['beers'],
+  components: {
+    // "beer-list-item": BeerListItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
